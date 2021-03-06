b4d45f4ca648906c30726075edadf7f2cf648a37
Conal Walsh <conal.walsh1@gmail.com>
2021-03-06 18:50:36 +0000

    Added package.json, configured parcel, babel and dotenv

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 require('dotenv').config();
 const api_key = process.env.API_KEY;
+
+/**
+ * getWeather data from openweather api
+ * @param {int} long
+ * @param {int} lat
+ */
+const getWeather = async function (long, lat) {
+  const res = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api_key}`
+  );
+  const data = await res.json();
+  console.log(data);
+};
 
 // Get our long and lat location
 window.addEventListener('load', () => {
@@ -11,6 +26,7 @@ window.addEventListener('load', () => {
       console.log(position);
       long = position.coords.longitude;
       lat = position.coords.latitude;
+      getWeather(long, lat);
     });
   } else {
     alert('Hey you need to allow geolocation for this app to work');
