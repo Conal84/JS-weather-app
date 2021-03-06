a6b39cd092a1db40fa93742113242b95236e492f
Conal Walsh <conal.walsh1@gmail.com>
2021-03-06 12:51:03 +0000

    Added JS geolocation

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ window.addEventListener('load', () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       console.log(position);
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
     });
   } else {
     alert('Hey you need to allow geolocation for this app to work');
